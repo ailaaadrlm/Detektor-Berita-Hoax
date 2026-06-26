54e1e663372f08dac8a5c94846ade3fd49b23d48
ailaaadrlm <aiiderll17@gmail.com>
2026-06-26 19:58:17 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-import streamlit as st
 import os
 os.environ["PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION"] = "python"
+import streamlit as st
 import torch
 import re
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
